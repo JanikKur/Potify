b7f7f2d19e6c7a478ae86bc5dce4407fb692934b
JaniKur <janik.kurtz2@googlemail.com>
2022-08-11 13:52:35 +0200

    Added default values to settings page

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import '../assets/styles/pages/settings.css';
+import {useUser} from '../contexts/UserContext';
 
 export default function Settings() {
+
+    
+    const {currentUser} = useUser();
+
+    if(!currentUser) return null;
     return (
         <main>
             <div className="user-informations">
-                <h1 className="name-icon">J</h1>
-                <h1 className="name">Janik Kurtz</h1>
+                <h1 className="name-icon">{currentUser.username[0].toUpperCase()}</h1>
+                <h1 className="name">{currentUser.username}</h1>
             </div>
             <form className="settings">
                 <div className="form-group">
                     <label>Name</label>
-                    <input type="text" placeholder="Name" className="form-control" />
+                    <input type="text" placeholder="Name" defaultValue={currentUser.username} className="form-control" />
                 </div>
                 <div className="form-group">
                     <label>E-Mail</label>
-                    <input type="email" placeholder="E-Mail" className="form-control" />
+                    <input type="email" placeholder="E-Mail" defaultValue={currentUser.email} className="form-control" />
                 </div>
                 <details className="new-password-wrapper">
                     <summary>New Password</summary>
