74d05c1907c6bbbb508da44ed0f97a2ef24f0897
Janik <janik.kurtz2@googlemail.com>
2022-08-12 17:25:16 +0200

    Added Search input to navigation

--- src/layouts/Navigation.js
+++ src/layouts/Navigation.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 import {AiOutlineSearch, AiOutlineMenu} from 'react-icons/ai';
 import '../assets/styles/layouts/navigation.css';
 import UserIcon from '../components/UserIcon';
@@ -8,7 +8,17 @@ import {useUser} from '../contexts/UserContext';
 export default function Navigation() {
 
     const [showMenu, setShowMenu] = useState(false);
+    const [showSearch, setShowSearch] = useState(false);
+    const searchRef = useRef();
     const {currentUser, logout} = useUser();
+
+    const navigate = useNavigate();
+
+    function submit(e){
+        e.preventDefault();
+        setShowSearch(false);
+        navigate(`/search?q=${searchRef.current.value}`);
+    }
 
     return (
         <nav className="navigation">
@@ -24,7 +34,12 @@ export default function Navigation() {
                 {currentUser && <button className="logout-button" onClick={logout}>Logout</button>}
             </div>
             <div className="right">
-                    <button className="search-button"><AiOutlineSearch/></button>
+                    <div className="navigation-search">
+                        <form className={`navigation-search-form ${showSearch ? 'show' : ''}`} onSubmit={submit}>
+                            <input type="text" className='navigation-search-input' ref={searchRef} />
+                        </form>
+                        <button className="search-button" onClick={() => {setShowSearch(!showSearch); searchRef.current.focus()}}><AiOutlineSearch/></button>
+                    </div>
                     {currentUser ? <UserIcon name={currentUser.username[0].toUpperCase()} /> : <div><Link to="/login" className="nav-link">Log In</Link>/<Link to="/register" className="nav-link">Sign Up</Link></div>}
             </div>
         </nav>
