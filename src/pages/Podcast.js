8cdc72e8f7eff8a0c600bcfcebc3a26b932ff84f
Janik <janik.kurtz2@googlemail.com>
2022-08-15 16:39:06 +0200

    Removed now all backend links

--- src/pages/Podcast.js
+++ src/pages/Podcast.js
@@ -57,7 +57,7 @@ export default function Podcast() {
             </div>
             <div className="episodes-list">
                 <h4>Alle Episoden:</h4>
-                {!podcast.episodes.length ? 'No Episodes yet' : podcast.episodes.map((episode, idx) => <Episode key={idx} onClick={link => {updateEpisode(link); setCurrentTitle(episode.title)}} currentEpisode={currentEpisode} episodeLink={`/api/v1/podcast/play/${episode.fileLinks[0]}`} image={`${process.env.REACT_APP_BACKEND_URL}${podcast.fileLinks[0]}`} title={episode.title} />)}
+                {!podcast.episodes.length ? 'No Episodes yet' : podcast.episodes.map((episode, idx) => <Episode key={idx} onClick={link => {updateEpisode(link); setCurrentTitle(episode.title)}} currentEpisode={currentEpisode} episodeLink={`/api/v1/podcast/play/${episode.fileLinks[0]}`} image={`${podcast.fileLinks[0]}`} title={episode.title} />)}
             </div>
         </main>
         </>
